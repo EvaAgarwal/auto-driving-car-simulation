e709afe2b8e6dc6ff39bbde278567642ed0ccf9b
Eva Agarwal <68182319+EvaAgarwal@users.noreply.github.com>
2025-01-08 10:00:27 +0800

    Update test_car.py

--- test_car.py
+++ test_car.py
@@ -1,1 +1,21 @@
+import unittest
+from car import Car
 
+class TestCar(unittest.TestCase):
+    def test_move(self):
+        car = Car("A", 0, 0, "N")
+        car.move()
+        self.assertEqual((car.x, car.y), (0, 1))
+
+    def test_rotate_left(self):
+        car = Car("A", 0, 0, "N")
+        car.rotate_left()
+        self.assertEqual(car.direction, "W")
+
+    def test_rotate_right(self):
+        car = Car("A", 0, 0, "N")
+        car.rotate_right()
+        self.assertEqual(car.direction, "E")
+
+if __name__ == "__main__":
+    unittest.main()
